80a613a1cec1d2127fdc897c6eb65015a8bce463
ligaya <ligaya96@gmail.com>
2021-01-13 19:15:04 -0800

    starting JS file

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var length = [ "setLength <8 || setLength > 128" ];
+
+// set password possibilities
+var values= [ "abcdefghijklmnopqrstuvwxyz0123456789 ".split("") ]; 
+
+var password= "";
+
+// loop for varierty of password possibilites
+for (var i=0; i <=generateBtn; i++)
+  password=password + values.concat(Math.floor(Math.random() * values.length))
+
+// promt password critria 
+
+// prompt for the length of password
+if (setLength <8 || setLength > 128) {
+}
+
+// prompt for charactor types ie uppper case/ lowercases
+
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +31,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
